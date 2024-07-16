3c1a5ab524bee126f9d2e70a06220ebbcb35f7c9
arvee0220 <rowellverdejo@gmail.com>
2024-07-16 06:19:56 -0400

    Commit boiler plate

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
 import Image from "next/image";
+import Profile from "../public/random_user.jpg";
 
 export default function Home() {
-	return <main></main>;
+	return (
+		<main>
+			<div>Cover Photo</div>
+			<div>Profile</div>
+
+			<div className="w-32 h-56 overflow-hidden rounded-full">
+				<Image
+					src={Profile}
+					height={448}
+					width={256}
+					alt="profile"
+					className="object-cover w-full h-full"
+				/>
+			</div>
+		</main>
+	);
 }
